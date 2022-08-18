6d0739c8ffc65e4cb6205fb75ee8f770eace6556
Robby Thompson <robsdudeson@gmail.com>
2022-08-18 15:18:33 -0400

    setup PubSub for updates

--- lib/live_view_todos_web/live/todo_live.ex
+++ lib/live_view_todos_web/live/todo_live.ex
@@ -4,12 +4,24 @@ defmodule LiveViewTodosWeb.TodoLive do
   alias LiveViewTodos.Todos
 
   def mount(_params, _session, socket) do
+    Todos.subscribe()
+
     {:ok, fetch(socket)}
   end
 
   def handle_event("add", %{"todo" => todo}, socket) do
     Todos.create_todo(todo)
 
+    {:noreply, socket}
+  end
+
+  def handle_event("toggle_done", %{"id" => id}, socket) do
+    todo = Todos.get_todo!(id)
+    Todos.update_todo(todo, %{done: !todo.done})
+    {:noreply, socket}
+  end
+
+  def handle_info({Todos, [:todo | _], _}, socket) do
     {:noreply, fetch(socket)}
   end
 
