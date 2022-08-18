061061867d69f76a70f79c4685627d3fa4c378a0
Robby Thompson <robsdudeson@gmail.com>
2022-08-18 15:11:32 -0400

    Add Live View to create and show TODOs

---
+++ lib/live_view_todos_web/live/todo_live.ex
@@ -0,0 +1,19 @@
+defmodule LiveViewTodosWeb.TodoLive do
+  use LiveViewTodosWeb, :live_view
+
+  alias LiveViewTodos.Todos
+
+  def mount(_params, _session, socket) do
+    {:ok, fetch(socket)}
+  end
+
+  def handle_event("add", %{"todo" => todo}, socket) do
+    Todos.create_todo(todo)
+
+    {:noreply, fetch(socket)}
+  end
+
+  defp fetch(socket) do
+    assign(socket, todos: Todos.list_todos())
+  end
+end
